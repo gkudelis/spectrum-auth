cdbaee9984c631d93741a4b84693fe36dc15a1e2
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2017-10-21 15:05:32 +0100

    Remove all of the clojure stuff

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject spectrum-auth "0.1.0-SNAPSHOT"
-  :description "Sends a waveform via phone call to authenticate a user"
-  :url "https://github.com/gkudelis/spectrum-auth"
-  :license {:name "MIT"
-            :url "https://mit-license.org/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.webbitserver/webbit  "0.4.15"]
-                 [org.clojure/data.json  "0.2.6"]])
--- src/spectrum_auth/core.clj
+++
@@ -1,6 +0,0 @@
-(ns spectrum-auth.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
